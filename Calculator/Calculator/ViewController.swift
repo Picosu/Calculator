c8f27d43e1d5e60eda46b32aad3121a7e3629847
Maxence de Cussac <Picsou@iMac-de-Maxence.bouyguesbox.fr>
2015-02-19 10:12:35 +0100

    création du projet

---
+++ Calculator/Calculator/ViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Maxence de Cussac on 19/02/2015.
+//  Copyright (c) 2015 Maxence de Cussac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var display: UILabel!
+    
+    var userIsInTheMiddleOfTypingANumber: Bool = false
+    
+    @IBAction func appendDigit(sender: UIButton) {
+        let digit = sender.currentTitle!
+        if userIsInTheMiddleOfTypingANumber {
+            display.text = display.text! + digit
+        } else {
+            display.text = digit
+            userIsInTheMiddleOfTypingANumber = true
+        }
+    }
+
+    var operandStack = Array<Double>()
+    
+    @IBAction func enter() {
+        userIsInTheMiddleOfTypingANumber = false
+        operandStack.append(displayValue)
+        println("operandStack : \(operandStack)")
+    }
+    
+    var displayValue: Double {
+        get {
+            return NSNumberFormatter().numberFromString(display.text!)!.doubleValue
+        }
+        set {
+            display.text = "\(newValue)"
+            userIsInTheMiddleOfTypingANumber = false
+        }
+    }
+}
+
