8a52483ae3044e58f1d2ced60258a964ae153b34
Maxence DE CUSSAC <decussac.m@sfeir.com>
2017-03-15 18:50:05 +0100

    Ajout de vues custom

---
+++ Calculator/Calculator/BorderedButton.swift
@@ -0,0 +1,21 @@
+//
+//  BorderedButton.swift
+//  Calculator
+//
+//  Created by Maxence DE CUSSAC on 14/03/2017.
+//  Copyright © 2017 Maxence de Cussac. All rights reserved.
+//
+
+import UIKit
+
+class BorderedButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ Calculator/Calculator/String+Extension.swift
@@ -0,0 +1,9 @@
+//
+//  String+Extension.swift
+//  Calculator
+//
+//  Created by Maxence DE CUSSAC on 14/03/2017.
+//  Copyright © 2017 Maxence de Cussac. All rights reserved.
+//
+
+import Foundation
